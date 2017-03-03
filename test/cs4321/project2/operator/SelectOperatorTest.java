0497f236533e0b01c62f910612393a4b0bc61615
Yangyi Hao <ronhao2013@126.com>
2017-03-03 17:09:28 -0500

    added select tests

--- test/cs4321/project2/operator/SelectOperatorTest.java
+++ test/cs4321/project2/operator/SelectOperatorTest.java
@@ -25,77 +25,75 @@ public class SelectOperatorTest {
 	/**
 	 * Test method for {@link cs4321.project2.operator.SelectOperator#getNextTuple()}.
 	 */
-//	@Test
-//	public void testGetNextTuple() {
-//		System.out.println("----------------------");
-//		System.out.println("| testGetNextTuple() |");
-//		System.out.println("----------------------");
-//		try {
-//			String inputdir = "testFolder";
-//			CCJSqlParser parser = new CCJSqlParser(new FileReader(inputdir + "/queries_custom0.sql"));
-//			Statement statement;
-//			Catalog.getInstance(inputdir);
-//			while ((statement = parser.Statement()) != null) {
-//				Select select = (Select) statement;
-//				PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
-//				FromItem fromItem = plainSelect.getFromItem();
-//				ScanOperator sOp = new ScanOperator(fromItem,inputdir);
-//				Expression expression = plainSelect.getWhere();
-//				System.out.print("----- New Query: ");
-//				System.out.println(expression.toString() + " -----");
-//				SelectOperator seOp = new SelectOperator(sOp, fromItem, expression);
-//				seOp.getNextTuple().print(); // should print 1,200,50,
-//				seOp.getNextTuple().print(); // should print 2,200,200,
-//				seOp.getNextTuple().print(); // should print 3,100,105
-//				seOp.getNextTuple().print(); // should print 3,100,50
-//				seOp.getNextTuple().print(); // should print 5,100,500
-//				seOp.getNextTuple().print(); // should print 6,300,400
-//			}			
-//		}
-//		catch (Exception e) {
-//			e.printStackTrace();
-//			System.err.println(e.getMessage());
-//			fail();
-//		}
-//	}
-//
-//	/**
-//	 * Test method for {@link cs4321.project2.operator.SelectOperator#reset()}.
-//	 */
-//	@Test
-//	public void testReset() {
-//		System.out.println("---------------");
-//		System.out.println("| testReset() |");
-//		System.out.println("---------------");
-//		try {
-//			String inputdir = "testFolder";
-//			CCJSqlParser parser = new CCJSqlParser(new FileReader(inputdir + "/queries_custom0.sql"));
-//			Statement statement;
-//			Catalog.getInstance(inputdir);
-//			while ((statement = parser.Statement()) != null) {
-//				Select select = (Select) statement;
-//				PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
-//				FromItem fromItem = plainSelect.getFromItem();
-//				ScanOperator sOp = new ScanOperator(fromItem,inputdir);
-//				Expression expression = plainSelect.getWhere();
-//				System.out.print("----- New Query: ");
-//				System.out.println(expression.toString() + " -----");
-//				SelectOperator seOp = new SelectOperator(sOp, fromItem, expression);
-//				seOp.getNextTuple().print(); // should print 1,200,50,
-//				seOp.getNextTuple().print(); // should print 2,200,200,
-//				seOp.getNextTuple().print(); // should print 3,100,105
-//				seOp.reset();
-//				seOp.getNextTuple().print(); // should print 1,200,50
-//				seOp.getNextTuple().print(); // should print 2,200,200
-//				seOp.getNextTuple().print(); // should print 3,100,105
-//			}			
-//		}
-//		catch (Exception e) {
-//			e.printStackTrace();
-//			System.err.println(e.getMessage());
-//			fail();
-//		}
-//	}
+	@Test
+	public void testGetNextTuple() {
+		System.out.println("----------------------");
+		System.out.println("| testGetNextTuple() |");
+		System.out.println("----------------------");
+		try {
+			String inputdir = "testFolder";
+			CCJSqlParser parser = new CCJSqlParser(new FileReader(inputdir + "/queries_custom0.sql"));
+			Statement statement;
+			Catalog.getInstance(inputdir);
+			while ((statement = parser.Statement()) != null) {
+				Select select = (Select) statement;
+				PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
+				FromItem fromItem = plainSelect.getFromItem();
+				ScanOperator sOp = new ScanOperator(fromItem,inputdir);
+				Expression expression = plainSelect.getWhere();
+				System.out.print("----- New Query: ");
+				System.out.println(expression.toString() + " -----");
+				SelectOperator seOp = new SelectOperator(sOp, fromItem, expression);
+				Tuple nextTuple = seOp.getNextTuple();
+				while (nextTuple != null) {
+					nextTuple.print(); // Should print all the qualified tuples for the current query
+					nextTuple = seOp.getNextTuple();
+				}
+			}			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+			fail();
+		}
+	}
+
+	/**
+	 * Test method for {@link cs4321.project2.operator.SelectOperator#reset()}.
+     */	 
+	@Test
+	public void testReset() {
+		System.out.println("---------------");
+		System.out.println("| testReset() |");
+		System.out.println("---------------");
+		try {
+			String inputdir = "testFolder";
+			CCJSqlParser parser = new CCJSqlParser(new FileReader(inputdir + "/queries_custom0.sql"));
+			Statement statement;
+			Catalog.getInstance(inputdir);
+			while ((statement = parser.Statement()) != null) {
+				Select select = (Select) statement;
+				PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
+				FromItem fromItem = plainSelect.getFromItem();
+				ScanOperator sOp = new ScanOperator(fromItem,inputdir);
+				Expression expression = plainSelect.getWhere();
+				System.out.print("----- New Query: ");
+				System.out.println(expression.toString() + " -----");
+				SelectOperator seOp = new SelectOperator(sOp, fromItem, expression);
+				Tuple nextTuple = seOp.getNextTuple();
+				if (nextTuple != null) {
+					nextTuple.print();
+					seOp.reset();
+					seOp.getNextTuple().print(); // Should print the first qualified tuple twice, if there is any qualified tuples.
+				}
+			}			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+			fail();
+		}
+	}
 
 	/**
 	 * Test method for {@link cs4321.project2.operator.SelectOperator#dump()}.
