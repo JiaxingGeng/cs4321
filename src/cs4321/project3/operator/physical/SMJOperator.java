12a561aba5dd199199ce3ad5634f696dd70d36bb
unknown <>
2017-04-13 15:07:11 -0400

    cleaned

--- src/cs4321/project3/operator/physical/SMJOperator.java
+++ src/cs4321/project3/operator/physical/SMJOperator.java
@@ -35,11 +35,8 @@ public class SMJOperator extends Operator {
 		revertPoint = 0;
 		currRightIndex = 0;
 		
-		// Step 1: figure out how to sort op1 and op2
 		String[] columns1 = op1.getColumns();
 		String tableAliasOrName1 = columns1[0].split("\\.")[0];
-		//System.out.println(tableAliasOrName1);
-		//System.out.println(Arrays.toString(columns1));
 		String[] columns2 = op2.getColumns();
 		String tableAliasOrName2 = columns2[0].split("\\.")[0];
 		super.columns = new String[columns1.length + columns2.length];
@@ -61,55 +58,16 @@ public class SMJOperator extends Operator {
 			orderByElement.setExpression(orderByElementsInArray.get(1).get(i));
 			orderByElements2.add(orderByElement);
 		}
-		/*
-		for (int i = 0; i < orderByElements1.size(); i++) {
-			System.out.println(orderByElements1.get(i).toString());
-		}
-		for (int i = 0; i < orderByElements2.size(); i++) {
-			System.out.println(orderByElements2.get(i).toString());
-		}
-		*/
 		so1 = new SortOperator(op1, orderByElements1, new ArrayList<SelectItem> ());
-		//so1.dump();
-		//System.out.println("*******************************");
 		so2 = new SortOperator(op2, orderByElements2, new ArrayList<SelectItem> ());
-		//so2.dump();
-		//System.out.println("*******************************");
 		leftTuple = so1.getNextTuple();
 		rightTuple = so2.getNextTuple();
 		currRightIndex++;
-		//System.out.println(tableAliasOrName2);
-		//System.out.println(Arrays.toString(columns2));
-		//String exp_string = expression.toString();
-		//System.out.println(exp_string);
-		
-//		leftTuple.print();
-//		so1.dump();
-//		System.out.println("*******************************");
-//		rightTuple.print();
-//		so2.dump();
-//		System.out.println("*******************************");
-//		so1.reset();
-//		so1.getNextTuple();
-//		so2.reset();
-//		so2.getNextTuple();
 		
 	}
 
 	@Override
 	public Tuple getNextTuple() throws IOException {
-		/*
-		System.out.print("the left tuple is: ");
-		if (leftTuple != null) {
-			leftTuple.print();
-		}
-		else System.out.println("null");
-		System.out.print("the right tuple is: ");
-		if (rightTuple != null) {
-			rightTuple.print();
-		}
-		else System.out.println("null");
-		*/
 		if (leftTuple == null && rightTuple == null) {
 			needToRevert = false;
 			return null;
@@ -123,7 +81,6 @@ public class SMJOperator extends Operator {
 				rightTuple = resetRight(revertPoint);
 				currRightIndex = revertPoint;
 				leftTuple = so1.getNextTuple();
-				//leftTuple.print();
 				return this.getNextTuple();
 			}
 			else {
@@ -138,9 +95,6 @@ public class SMJOperator extends Operator {
 			rightTuple = so2.getNextTuple();
 			currRightIndex++;
 			
-			//System.out.print("outputting ");
-			//leftTuple.joins(tempRightTuple).print();
-			
 			return leftTuple.joins(tempRightTuple);
 		}
 		else {
@@ -149,13 +103,11 @@ public class SMJOperator extends Operator {
 				rightTuple = resetRight(revertPoint);
 				currRightIndex = revertPoint;
 				leftTuple = so1.getNextTuple();
-				//leftTuple.print();
 				return this.getNextTuple();
 			}
 			else {
 				if (compare(leftTuple, rightTuple) < 0) {
 					leftTuple = so1.getNextTuple();
-					//leftTuple.print();
 					return this.getNextTuple();
 				}
 				if (compare(leftTuple, rightTuple) > 0) {
@@ -186,47 +138,26 @@ public class SMJOperator extends Operator {
 	 * @return
 	 */
 	public int compare(Tuple tuple1, Tuple tuple2) {
-		/*
-		System.out.print("comparing ");
-		tuple1.print();
-		System.out.print("and ");
-		tuple2.print();
-		*/
-		//tuple1.print();
-		//tuple2.print();
 		HashMap<String, Integer> hashMap1 = op1.getColumnsHash();
-		//System.out.println(hashMap1);
 		HashMap<String, Integer> hashMap2 = op2.getColumnsHash();
-		//System.out.println(hashMap2);
 		for (int i = 0; i < orderByElements1.size(); i++) {
 			Column column1 = (Column) orderByElements1.get(i).getExpression();
 			Column column2 = (Column) orderByElements2.get(i).getExpression();
 			String columnName1 = orderByElements1.get(i).toString();
 			String alias1 = column1.getTable().getAlias();
 			if (alias1 != null) columnName1 = alias1 + "." + columnName1.split("\\.")[1];
-			//System.out.println(columnName1);
 			String columnName2 = orderByElements2.get(i).toString();
 			String alias2 = column2.getTable().getAlias();
 			if (alias2 != null) columnName2 = alias2 + "." + columnName2.split("\\.")[1];
-			//System.out.println(columnName2);
 			String attr1 = tuple1.getElement(hashMap1.get(columnName1));
-			//System.out.println("attr1 is: " + attr1);
-			//System.out.println(hashMap2.get(columnName2));
 			String attr2 = tuple2.getElement(hashMap2.get(columnName2));
-			//String attr2 = tuple2.getElement(1);
-			//System.out.println("attr2 is: " + attr2);
-			//if (attr1.compareTo(attr2) < 0) {
 			if (Integer.parseInt(attr1) < Integer.parseInt(attr2)) {
-				//System.out.println("the result is: -1");
 				return -1;
 			}
-			//if (attr1.compareTo(attr2) > 0) {
 			if (Integer.parseInt(attr1) > Integer.parseInt(attr2)) {
-				//System.out.println("the result is: 1");
 				return 1;
 			}
 		}
-		//System.out.println("the result is: 0");
 		return 0;
 	}
 
--- test/cs4321/project3/operator/physical/SMJOperatorTest.java
+++ test/cs4321/project3/operator/physical/SMJOperatorTest.java
@@ -23,16 +23,6 @@ import net.sf.jsqlparser.schema.Table;
 
 public class SMJOperatorTest {
 
-//	@Test
-//	public void testGetNextTuple() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testReset() {
-//		fail("Not yet implemented");
-//	}
-
 	@Test
 	public void testSMJOperator() {
 		System.out.println("-----------------------");
@@ -47,23 +37,18 @@ public class SMJOperatorTest {
 		EqualsTo equalsToExp1 = new EqualsTo();
 		equalsToExp1.setLeftExpression(leftExpCol1);
 		equalsToExp1.setRightExpression(rightExpCol1);
-		//equalsToExp1.accept(new SMJExpressionDeParser());
 		Column leftExpCol2 = new Column(tl, "B");
 		Column rightExpCol2 = new Column(tr, "H");
 		EqualsTo equalsToExp2 = new EqualsTo();
 		equalsToExp2.setLeftExpression(leftExpCol2);
 		equalsToExp2.setRightExpression(rightExpCol2);
-		//equalsToExp2.accept(new SMJExpressionDeParser());
 		AndExpression andExp = new AndExpression();
 		andExp.setLeftExpression(equalsToExp1);
 		andExp.setRightExpression(equalsToExp2);
-		//andExp.accept(new SMJExpressionDeParser());
 		try{ Catalog.getInstance("input");
 		ScanOperator sol = new ScanOperator(tl);
 		ScanOperator sor = new ScanOperator(tr);
 		SMJOperator smjo = new SMJOperator(sol, sor, andExp);
-		//smjo.dump();
-		//jo.dump();
 		} catch (IOException e) {}
 	}
 	
