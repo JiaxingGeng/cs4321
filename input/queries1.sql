66e82ac8b3f690695c3fb5998ea0ce80514ce852
Jiaxing Geng <jiaxinggeng@Jiaxings-MBP.lan>
2017-04-13 23:12:20 -0400

    passed all given tests

--- input/queries1.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM Sailors, Reserves WHERE Sailors.A = Reserves.G;
-SELECT * FROM Sailors, Reserves, Boats WHERE Sailors.A = Reserves.G AND Reserves.H = Boats.D;
-SELECT * FROM Sailors S, Reserves R, Boats B WHERE S.A = R.G AND R.H = B.D ORDER BY S.C;
-SELECT DISTINCT * FROM Sailors S, Reserves R, Boats B WHERE S.A = R.G AND R.H = B.D ORDER BY S.C;
